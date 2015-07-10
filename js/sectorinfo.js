22aeb7868db5bdd6d850e6b9c539632962a6fe9e
samuelestabrook <samuel.estabrook@gmail.com>
2015-07-10 13:57:44 -0400

    images and sector data
    still have to tune the sector data to the CSS and javascript - there
    are multiple links per sector

--- js/sectorinfo.js
+++ js/sectorinfo.js
@@ -1,37 +1,79 @@
 var sectorInfo = [
-  {
-    "sector":"Cholera",
-    "blurb":"Tattooed bitters drinking vinegar flannel vinyl four loko eiusmod tousled quinoa roof party, Thundercats pickled. Tilde elit Schlitz PBR pug, sustainable raw denim adipisicing kale chips laboris fixie tousled. Stumptown Brooklyn slow-carb lumbersexual Thundercats Shoreditch. Actually disrupt blog, 90's listicle pour-over stumptown Tumblr cupidatat ad Brooklyn Shoreditch irony. Nihil beard before they sold out sunt aliqua, Banksy qui incididunt authentic polaroid bespoke vero swag. Et sartorial nihil, proident before they sold out roof party sed nostrud twee qui. Reprehenderit officia placeat, Etsy butcher Tumblr shabby chic.",
-    "link":"http://unhcr.github.io/Humanitarian-Font/#icons-sector"
-  },
-  {
-    "sector":"Disaster Risk Reduction",
-    "blurb":"Skateboard odio master cleanse exercitation, Intelligentsia distillery YOLO iPhone kale chips ad cupidatat dolore. Echo Park 3 wolf moon semiotics, leggings voluptate nulla asymmetrical. Vice umami mumblecore, skateboard cold-pressed Etsy Portland sriracha. Nulla Thundercats cred, paleo messenger bag mollit McSweeney's. Food truck adipisicing art party plaid dreamcatcher. Cold-pressed actually Echo Park, viral Intelligentsia banjo plaid bespoke irure. Veniam Helvetica vinyl, cronut 90's Austin Schlitz nisi et normcore forage hoodie four dollar toast.",
-    "link":"https://trello.com"
-  },
-  {
-    "sector":"Emergency Response",
-    "blurb":"Selvage chambray occupy readymade lomo. Single-origin coffee migas American Apparel small batch put a bird on it, cred forage Carles. Roof party whatever lo-fi, flexitarian wolf viral Williamsburg. PBR&B cliche crucifix +1, sriracha readymade listicle McSweeney's ugh. Meggings brunch VHS fanny pack. Crucifix lumbersexual Thundercats, aesthetic stumptown farm-to-table 90's you probably haven't heard of them single-origin coffee cliche kogi Helvetica Etsy pug sustainable. Cray four dollar toast cardigan disrupt, church-key Odd Future Echo Park slow-carb meh lomo High Life +1 irony drinking vinegar craft beer.",
-    "link":"http://getbootstrap.com/"
-  },
-  {
-    "sector":"Health",
-    "blurb":"Chillwave cold-pressed letterpress Shoreditch tofu Carles. Thundercats photo booth Austin tattooed. Literally hoodie kale chips pork belly cliche gluten-free butcher, keytar fingerstache shabby chic. Gastropub brunch fap PBR, forage street art jean shorts lomo Helvetica selvage Schlitz asymmetrical next level freegan. Aesthetic hoodie XOXO flexitarian, McSweeney's typewriter farm-to-table hashtag gluten-free VHS shabby chic. Banksy before they sold out Shoreditch semiotics. Vinyl cray kogi Banksy.",
-    "link":"http://unhcr.github.io/Humanitarian-Font/#icons-sector"
-  },
-  {
-    "sector":"Livelihoods",
-    "blurb":"Tattooed bitters drinking vinegar flannel vinyl four loko eiusmod tousled quinoa roof party, Thundercats pickled. Tilde elit Schlitz PBR pug, sustainable raw denim adipisicing kale chips laboris fixie tousled. Stumptown Brooklyn slow-carb lumbersexual Thundercats Shoreditch. Actually disrupt blog, 90's listicle pour-over stumptown Tumblr cupidatat ad Brooklyn Shoreditch irony. Nihil beard before they sold out sunt aliqua, Banksy qui incididunt authentic polaroid bespoke vero swag. Et sartorial nihil, proident before they sold out roof party sed nostrud twee qui. Reprehenderit officia placeat, Etsy butcher Tumblr shabby chic.",
-    "link":"https://trello.com"
-  },
-  {
-    "sector":"Shelter",
-    "blurb":"Skateboard odio master cleanse exercitation, Intelligentsia distillery YOLO iPhone kale chips ad cupidatat dolore. Echo Park 3 wolf moon semiotics, leggings voluptate nulla asymmetrical. Vice umami mumblecore, skateboard cold-pressed Etsy Portland sriracha. Nulla Thundercats cred, paleo messenger bag mollit McSweeney's. Food truck adipisicing art party plaid dreamcatcher. Cold-pressed actually Echo Park, viral Intelligentsia banjo plaid bespoke irure. Veniam Helvetica vinyl, cronut 90's Austin Schlitz nisi et normcore forage hoodie four dollar toast.",
-    "link":"http://getbootstrap.com/"
-  },
-  {
-    "sector":"Water and Sanitation",
-    "blurb":"Chillwave cold-pressed letterpress Shoreditch tofu Carles. Thundercats photo booth Austin tattooed. Literally hoodie kale chips pork belly cliche gluten-free butcher, keytar fingerstache shabby chic. Gastropub brunch fap PBR, forage street art jean shorts lomo Helvetica selvage Schlitz asymmetrical next level freegan. Aesthetic hoodie XOXO flexitarian, McSweeney's typewriter farm-to-table hashtag gluten-free VHS shabby chic. Banksy before they sold out Shoreditch semiotics. Vinyl cray kogi Banksy.",
-    "link":"http://unhcr.github.io/Humanitarian-Font/#icons-sector"
-  }
+   {
+      "sector":"Emergency Relief",
+      "story_name":"emergency_relief",
+      "blurb":"After the earthquake, ready-to-eat-meals were provided to survivors, like Etienne Radine and Louis Anouse, by the American Red Cross.",
+      "link_1_name":"Haiti Earthquake 5-year Update",
+      "link_1":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m51740138_HaitiFiveYearUpdate-2.pdf",
+      "link_2_name":"Haiti Earthquake 4-year Update",
+      "link_2":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m27740093_Haiti_Four_Year_Update.pdf",
+      "link_3_name":"Haiti Earthquake 1-year Update",
+      "link_3":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m3140113_HaitiEarthquake_OneYearReport.pdf"
+   },
+   {
+      "sector":"Shelter",
+      "story_name":"shelter",
+      "blurb":"In addition to providing more permanent shelter solutions, retrofitting homes and repairing infrastructure, training has been provided to construction workers, like Aldomoro Victor (center) and Pierre- Richard Etienne, on safer building practices that will make houses more resistant to natural disasters.",
+      "link_1_name":"Fact Sheet: Behind the Numbers: American Red Cross Housing Solutions in Haiti",
+      "link_1":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m51740178_American_Red_Cross_Housing_in_Haiti.pdf",
+      "link_2_name":"Fact Sheet: Improving Housing in Haiti",
+      "link_2":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m44140254_Improving_Housing_in_Haiti.pdf",
+      "link_3_name":"",
+      "link_3":""
+   },
+   {
+      "sector":"Health",
+      "story_name":"health",
+      "blurb":"American Red Cross has helped repair, construct and operate eight hospitals and clinics, including partial funding of Mirebalais University Hospital.",
+      "link_1_name":"Fact Sheet: Preventing Disease and Improving Healthcare",
+      "link_1":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m42240140_Preventing_Disease_and_Improving_Healthcare_Factsheet.pdf",
+      "link_2_name":"Read about a new hospital opening, with support from Red Cross",
+      "link_2":"http://www.redcross.org/news/article/Mirebalais-Hospital-Opens-New-Units-to-serve-Haitians",
+      "link_3_name":"",
+      "link_3":""
+   },
+   {
+      "sector":"Cholera Prevention",
+      "story_name":"cholera",
+      "blurb":"Prevention efforts include vaccinations, water purification tablets, as well as distribution of oral dehydration tablets. The American Red Cross provided more than 70% of the funds needed for Haiti's first cholera vaccine, which reached 90,000 people.",
+      "link_1_name":"Read about Red Cross programs to eradicate Cholera",
+      "link_1":"http://www.redcross.org/news/article/Red-Cross-Continues-to-Fight-Cholera-in-Haiti",
+      "link_2_name":"",
+      "link_2":"",
+      "link_3_name":"",
+      "link_3":""
+   },
+   {
+      "sector":"Water & Sanitation",
+      "story_name":"watsan",
+      "blurb":"We’ve helped ensure that families have access to clean water and sanitation systems, as well as large scale public education campaigns that encourage safe hygiene. Here, workers build a septic tank for newly built housing units in Morne Lazare, Pétion-Ville.",
+      "link_1_name":"Read about Red Cross's extended emergency support for clean water",
+      "link_1":"http://www.redcross.org/news/article/Red-Cross-Helping-Haiti-Deliver-Water",
+      "link_2_name":"",
+      "link_2":"",
+      "link_3_name":"",
+      "link_3":""
+   },
+   {
+      "sector":"Livelihoods",
+      "story_name":"livelihoods",
+      "blurb":"We seek to improve household and community stability through savings programs, conditional loans and vocational training. Ginette Antenor, the owner of Cymara’s beauty salon, is in one such program for entrepreneurs that helps hone business and marketing skills through training and support.",
+      "link_1_name":"Read about Red Cross supported Entrepreneurs in Haiti",
+      "link_1":"http://www.redcross.org/news/article/In-Haiti-Entrepreneurs-Brush-Up-Their-Business-Skills",
+      "link_2_name":"",
+      "link_2":"",
+      "link_3_name":"",
+      "link_3":""
+   },
+   {
+      "sector":"Disaster Preparedness",
+      "story_name":"preparedness",
+      "blurb":"The American Red Cross has helped families, like Dalida Cadaste’s, create customized emergency plans, so they can be ready to evacuate their homes in case of hurricanes, tropical storms or other emergencies.",
+      "link_1_name":"Fact Sheet: Preparing for Disasters",
+      "link_1":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m28640086_Preparing_for_Disasters_in_Haiti_-_February_2014.pdf",
+      "link_2_name":"",
+      "link_2":"",
+      "link_3_name":"",
+      "link_3":""
+   }
 ];

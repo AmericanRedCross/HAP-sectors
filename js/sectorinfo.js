bcdd412ad8c2a002aa31c2a181f9f0467aa2e3bc
samuelestabrook <samuel.estabrook@gmail.com>
2015-07-10 15:32:35 -0400

    upload original CSV for story GPS point
    added to /data the original CSV with GPS points for stories. also
    reformatted the JSON for the sectors links.

--- js/sectorinfo.js
+++ js/sectorinfo.js
@@ -3,57 +3,60 @@ var sectorInfo = [
       "sector":"Emergency Response",
       "story_name":"emergency_relief",
       "blurb":"After the earthquake, ready-to-eat-meals were provided to survivors, like Etienne Radine and Louis Anouse, by the American Red Cross.",
-      "link_1_name":"Haiti Earthquake 5-year Update",
-      "link_1":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m51740138_HaitiFiveYearUpdate-2.pdf",
-      "link_2_name":"Haiti Earthquake 4-year Update",
-      "link_2":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m27740093_Haiti_Four_Year_Update.pdf",
-      "link_3_name":"Haiti Earthquake 1-year Update",
-      "link_3":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m3140113_HaitiEarthquake_OneYearReport.pdf"
+      "links": {
+         "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m51740138_HaitiFiveYearUpdate-2.pdf": "Haiti Earthquake 5-year Update",
+         "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m27740093_Haiti_Four_Year_Update.pdf": "Haiti Earthquake 4-year Update",
+         "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m3140113_HaitiEarthquake_OneYearReport.pdf": "Haiti Earthquake 1-year Update"
+      }
    },
    {
       "sector":"Shelter",
       "story_name":"shelter",
       "blurb":"In addition to providing more permanent shelter solutions, retrofitting homes and repairing infrastructure, training has been provided to construction workers, like Aldomoro Victor (center) and Pierre- Richard Etienne, on safer building practices that will make houses more resistant to natural disasters.",
-      "link_1_name":"Fact Sheet: Behind the Numbers: American Red Cross Housing Solutions in Haiti",
-      "link_1":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m51740178_American_Red_Cross_Housing_in_Haiti.pdf",
-      "link_2_name":"Fact Sheet: Improving Housing in Haiti",
-      "link_2":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m44140254_Improving_Housing_in_Haiti.pdf",
+      "links": {
+         "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m51740178_American_Red_Cross_Housing_in_Haiti.pdf": "Fact Sheet: Behind the Numbers: American Red Cross Housing Solutions in Haiti",
+         "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m44140254_Improving_Housing_in_Haiti.pdf": "Fact Sheet: Improving Housing in Haiti"
+      }
    },
    {
       "sector":"Health",
       "story_name":"health",
       "blurb":"American Red Cross has helped repair, construct and operate eight hospitals and clinics, including partial funding of Mirebalais University Hospital.",
-      "link_1_name":"Fact Sheet: Preventing Disease and Improving Healthcare",
-      "link_1":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m42240140_Preventing_Disease_and_Improving_Healthcare_Factsheet.pdf",
-      "link_2_name":"Read about a new hospital opening, with support from Red Cross",
-      "link_2":"http://www.redcross.org/news/article/Mirebalais-Hospital-Opens-New-Units-to-serve-Haitians",
+      "links": {
+         "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m42240140_Preventing_Disease_and_Improving_Healthcare_Factsheet.pdf": "Fact Sheet: Preventing Disease and Improving Healthcare",
+         "http://www.redcross.org/news/article/Mirebalais-Hospital-Opens-New-Units-to-serve-Haitians": "Read about a new hospital opening, with support from Red Cross"
+      }
    },
    {
       "sector":"Cholera",
       "story_name":"cholera",
       "blurb":"Prevention efforts include vaccinations, water purification tablets, as well as distribution of oral dehydration tablets. The American Red Cross provided more than 70% of the funds needed for Haiti's first cholera vaccine, which reached 90,000 people.",
-      "link_1_name":"Read about Red Cross programs to eradicate Cholera",
-      "link_1":"http://www.redcross.org/news/article/Red-Cross-Continues-to-Fight-Cholera-in-Haiti",
+      "links": {
+         "http://www.redcross.org/news/article/Red-Cross-Continues-to-Fight-Cholera-in-Haiti": "Read about Red Cross programs to eradicate Cholera"
+      }
    },
    {
       "sector":"Water and Sanitation",
       "story_name":"watsan",
       "blurb":"We’ve helped ensure that families have access to clean water and sanitation systems, as well as large scale public education campaigns that encourage safe hygiene. Here, workers build a septic tank for newly built housing units in Morne Lazare, Pétion-Ville.",
-      "link_1_name":"Read about Red Cross's extended emergency support for clean water",
-      "link_1":"http://www.redcross.org/news/article/Red-Cross-Helping-Haiti-Deliver-Water",
+      "links": {
+         "http://www.redcross.org/news/article/Red-Cross-Helping-Haiti-Deliver-Water": "Read about Red Cross's extended emergency support for clean water"
+      }
    },
    {
       "sector":"Livelihoods",
       "story_name":"livelihoods",
       "blurb":"We seek to improve household and community stability through savings programs, conditional loans and vocational training. Ginette Antenor, the owner of Cymara’s beauty salon, is in one such program for entrepreneurs that helps hone business and marketing skills through training and support.",
-      "link_1_name":"Read about Red Cross supported Entrepreneurs in Haiti",
-      "link_1":"http://www.redcross.org/news/article/In-Haiti-Entrepreneurs-Brush-Up-Their-Business-Skills",
+      "links": {
+         "http://www.redcross.org/news/article/In-Haiti-Entrepreneurs-Brush-Up-Their-Business-Skills": "Read about Red Cross supported Entrepreneurs in Haiti"
+      }
    },
    {
       "sector":"Disaster Risk Reduction",
       "story_name":"preparedness",
       "blurb":"The American Red Cross has helped families, like Dalida Cadaste’s, create customized emergency plans, so they can be ready to evacuate their homes in case of hurricanes, tropical storms or other emergencies.",
-      "link_1_name":"Fact Sheet: Preparing for Disasters",
-      "link_1":"http://www.redcross.org/images/MEDIA_CustomProductCatalog/m28640086_Preparing_for_Disasters_in_Haiti_-_February_2014.pdf",
+      "links": {
+         "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m28640086_Preparing_for_Disasters_in_Haiti_-_February_2014.pdf": "Fact Sheet: Preparing for Disasters"
+      }
    }
 ]

6e83f99e9b269dbd9eccd5c663e711d021297e24
samuelestabrook <samuel.estabrook@gmail.com>
2015-07-27 13:54:39 -0400

    fixes for finalization
    fixed background for stories (map wasn’t visible behind overflow div),
    fixed locations of stories, removed stories that were on top of each
    other (geographically)

--- js/stories_original.js
+++ js/stories_original.js
@@ -1,9 +1,20 @@
 var stories = [
   {
+    "title":"Martine",
+    "story_name":"martine",
+    "long":-72.332611084,
+    "lat":18.6006967637,
+    "story":"Martine Jozef's mother expressed her gratitude in the first year following the earthquake, \"With the financial support the American Red Cross gave us, I am sending her back to school again. Thank you.\" Donations to the American Red Cross kept people alive in the earthquake's aftermath and helped bring normalcy back into the lives of kids like Martine, who were able to return to school.",
+    "links": {
+      "http://www.redcross.org/news/article/American-Red%20Cross-Donor-Dollars-at-Work-in-Haiti": "American Red Cross: Donor Dollars at Work in Haiti",
+      "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m3140113_HaitiEarthquake_OneYearReport.pdf": "Haiti 1-year report"
+    }
+  },
+  {
     "title":"Jordanie",
     "story_name":"soccer",
-    "long":-72.311673,
-    "lat":18.6518095,
+    "long":-72.1362304688,
+    "lat":18.5538342857,
     "story":"Ten year-old Jordanie Derulus was trapped in the rubble of his house for two days following the earthquake. Once out of the rubble, Jordanie was brought to a hospital where doctors were forced to remove his right leg. While Jordanie never lost faith he would walk again, the leg that he has received from a prosthetics clinic co-funded by the American Red Cross and other global Red Cross network partners has allowed him to get back to normal. \"I can kick a soccer ball again, and walk,\" he explained, \"it's a wonderful thing.\"",
     "links": {
       "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m42240140_Preventing_Disease_and_Improving_Healthcare_Factsheet.pdf": "Improving Healthcare in Haiti",
@@ -13,8 +24,8 @@ var stories = [
   {
     "title":"Canaan",
     "story_name":"canaan",
-    "long":-72.2684237,
-    "lat":18.6518095,
+    "long":-72.2708129883,
+    "lat":18.6982854741,
     "story":"After the earthquake, many Haitian families decided to move out of Port-au-Prince and settle in Canaan, a rapidly emerging city about 30 minutes drive from the capital city. The American Red Cross is working with the community to ensure essential urban planning, basic neighborhood services, safe construction, income generation, disaster risk reduction, health, and environmental awareness. This approach is in accordance with the Government of Haiti's National Housing and Habitat Policy and leverages the resources which residents themselves bring to their community.",
     "links": {
       "http://www.usnews.com/news/articles/2015/01/12/5-years-later-haitians-find-hope-in-canaan-after-the-2010-earthquake": "The Promised Land: 5 Years Later, Haitians Find Hope in Canaan, US News & World Report"
@@ -32,6 +43,17 @@ var stories = [
     }
   },
   {
+    "title":"Lionel and Archelle",
+    "story_name":"father",
+    "long":-72.3271179199,
+    "lat":18.4796090558,
+    "story":"Archelle Clara, 8, and her father, Lionel Vilsaint sit together at their home in the neighborhood of Carrefour-Feuilles. \"I have received a lot of training from the American Red Cross in the past few years. If an emergency occurs, I know how to intervene,\" says Lionel. \"My family sleeps under the mosquito net every night. It's rainy season right now, so there is much to be aware of.\" In addition to improvements to infrastructure, health and livelihoods in this neighborhood, the American Red Cross is funding a wide range of disease prevention activities and preparing residents for any disasters that may come their way.",
+    "links": {
+      "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m28640086_Preparing_for_Disasters_in_Haiti_-_February_2014.pdf": "Preparing for Disasters in Haiti",
+      "http://www.redcross.org/news/article/Haitis-Families-Get-Ready-for-Hurricane-Season": "Haiti’s Families Get Ready for Hurricane Season"
+    }
+  },
+  {
     "title":"Angeni and Alexandra",
     "story_name":"soil",
     "long":-72.1241998,
@@ -44,12 +66,22 @@ var stories = [
   {
     "title":"Junior",
     "story_name":"junior",
-    "long":-72.4092495,
-    "lat":18.5345373,
+    "long":-72.4411010742,
+    "lat":18.5251897681,
     "story":"Junior Ducasse opens the door of his rented home in Carrefour. Junior was living in one of the many camps that dotted Port-au-Prince after the 2010 earthquake, but was very happy to move to a safe, earthquake-resistant home. \"I was in a camp, and I was really tired of it. I thought I was tired of life,\" he recalls. \"When I was in the camp, I was uneasy. Sometimes my things got stolen. I had a lot of problems, but since I came here, I feel very relaxed.\" A program sponsored by the American Red Cross, in partnership with CARE, helped homeowners repair and expand their houses, as well as make them more disaster-resistant. The homeowners who benefit from this program agree to provide a free room to former camp residents for 18 to 24 months, offering them a safe place to live as they continue to rebuild their lives.",
     "links": {
       "http://www.redcross.org/videos/In-Haiti-a-New-Home-Means-New-Hope": "In Haiti, a New Home Means New Hope",
       "http://www.redcross.org/news/article/Housing-in-Haiti-A-Second-Look": "Housing in Haiti: A Second Look"
+    }
+  },
+  {
+    "title":"Dominique",
+    "story_name":"dominique",
+    "long":-72.2158813477,
+    "lat":18.4665837676,
+    "story":"Artisan Dominique Milias is one of the 75 entrepreneurs who are honing their business and marketing skills through the American Red Cross. These business owners learn how to define their market, assess their advantage over the competition, and reach out to new customers. The program is part of an American Red Cross initiative to improve economic opportunities in Carrefour-Feuilles — a community badly damaged by the 2010 earthquake.",
+    "links": {
+      "http://www.redcross.org/news/article/In-Haiti-Entrepreneurs-Brush-Up-Their-Business-Skills": "In Haiti, Entrepreneurs Brush Up Their Business Skills"
     }
   },
   {
@@ -66,8 +98,8 @@ var stories = [
   {
     "title":"Landy",
     "story_name":"landy",
-    "long":-72.348253,
-    "lat":18.5242284,
+    "long":-72.2323608398,
+    "lat":18.5876807011,
     "story":"Landy used to stay home after sunset, too afraid to walk around or run errands in the darkness. But when the American Red Cross installed solar streetlights in her neighborhood of Campeche, everything changed. Landy now sells grilled corn under the streetlights without fearing for her safety. Her friends agree that they feel safer being out alone in the evenings and are less worried about falling on the rocky and uneven paths. The streetlights can reduce crime, mitigate gender-based violence, facilitate study after dark, and extend the hours during which residents can walk and conduct business in the neighborhood.",
     "links": {
       "http://www.redcross.org/images/MEDIA_CustomProductCatalog/m42240139_LAMIKA_Factsheet.pdf": "Read about our LAMIKA program in Campeche and surrounding neighborhoods"
